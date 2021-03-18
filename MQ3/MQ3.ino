e107e2ace10d0b11f21270566799b35e3ac263f1
isocxsi <57760113+isocxsi@users.noreply.github.com>
2021-03-18 21:32:49 +0800

    Add files via upload

--- MQ3/MQ3.ino
+++ MQ3/MQ3.ino
@@ -1,1 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  while(analogRead(A0)>600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int alco;
+  alco=analogRead(A0);
+  Serial.println(alco);
+  delay(1000);
+}
